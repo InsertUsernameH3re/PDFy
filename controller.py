76cb580fb03b2dc4dd2641d3683d2cd822f8ac4f
InsertUsernameH3re <spitz.michal@purkynka.cz>
2023-02-26 15:50:48 +0100

    feat: added pdf merger

--- controller.py
+++ controller.py
@@ -1,45 +1,68 @@
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtGui import QTextCursor
 from PyQt5.QtPrintSupport import QPrinter
-import fitz
 import PyPDF2
-
 class Controller:
 
         @classmethod
         def savePDF(cls, self):
-            file = QFileDialog.getSaveFileName(self, 'Save PDF', None, '.pdf')
-            if file[0] != "":
+            file, _ = QFileDialog.getSaveFileName(self, "Save PDF File", "", "Pdf Files (*.pdf);;All Files (*)")
+            if file != "":
                 printer = QPrinter(QPrinter.HighResolution)
                 printer.setOutputFormat(QPrinter.PdfFormat)
-                printer.setOutputFileName(file[0] + file[1])
+                printer.setOutputFileName(file)
                 self.editor.document().print_(printer)
         
         @classmethod
         def openPDF(cls, self):
-            #file = QFileDialog.getOpenFileName(self, 'Save PDF', None, '.pdf')
-            pdfReaderText = PyPDF2.PdfReader("hello.pdf")
-            pdfReaderImages = fitz.open("hello.pdf")
-            pdfText = ""
-            pdfImage = ""
-            for i in range(pdfReaderText.numPages):
-                page = pdfReaderImages[i]
-                print(page.get_images())
+            file, _ = QFileDialog.getOpenFileName(self, "Open PDF File", "", "Pdf Files (*.pdf);;All Files (*)")
+            pdf_reader = PyPDF2.PdfFileReader(file)
+            page = pdf_reader.getPage(0)
+            self.editor.insertPlainText(page.extract_text())
 
         @classmethod
         def changeFontSize(cls, self):
-            if self.fontSize.text() != "" and int(self.fontSize.text()) > 0:
-                self.editor.setFontPointSize(int(self.fontSize.text()))
+            if self.fontSize.value() != "" and int(self.fontSize.value()) > 0:
+                self.editor.setFontPointSize(int(self.fontSize.value()))
     
         @classmethod
         def checkSize(cls, self):
-            if self.editor.fontPointSize() != int(self.fontSize.text()):
-                self.editor.setFontPointSize(int(self.fontSize.text()))
+            print(self.editor.toHtml())
+            if self.editor.fontPointSize() != int(self.fontSize.value()):
+                self.editor.setFontPointSize(int(self.fontSize.value()))
 
         @classmethod
         def insertImage(cls, self):
-            file = QFileDialog.getOpenFileName(self, "Open Image", None, initialFilter="Image Files (*.png * .jpg * .bmp)")
+            file, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Image Files(*.jpg, *.png);;All Files (*)")
             if file[0] != "":
                 cursor = self.editor.textCursor()
-                cursor.insertImage("mobileapp.png")
-             
+                cursor.insertImage(file)
+#----------------------------------------------------PDF Merger------------------------------------------------------
+        filepaths = []
+        @classmethod
+        def getFilePaths(cls, self):
+            file, _ = QFileDialog.getOpenFileNames(self, "Open PDF Files", "", "Pdf Files (*.pdf);;All Files (*)")
+            self.labelProgress.setText("Loading files...")
+            for file in file:
+                cls.filepaths.append(file)
+            self.labelProgress.setText("Files loaded successfully")
+
+        @classmethod
+        def merge(cls, self):
+            progress = 0
+            self.labelProgress.setText("Merging files")
+            pdf_write = PyPDF2.PdfFileWriter()
+
+            for path in cls.filepaths:
+                progress += 90 / len(cls.filepaths)
+                self.progress.setValue(progress)
+                pdf_reader = PyPDF2.PdfFileReader(path)
+                for page in range(pdf_reader.getNumPages()):
+                    pdf_write.addPage(pdf_reader.getPage(page))
+
+            with open("merged.pdf", "wb") as f:
+                pdf_write.write(f)
+            self.progress.setValue(100)
+            self.labelProgress.setText("Output file: merged.pdf")
+#----------------------------------------------------------------------------------------------------------------------
+
--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-from main_window import *
+import main_window
+import merger
 from controller import Controller
 import sys
 
-from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import (QApplication, QMainWindow)
 
-onlyInt = QIntValidator()
-onlyInt.setRange(5, 99)
+html = "<h3>Table</h3><table border=1><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr><tr><td>Ernst Handel</td><td>Roland Mendel</td><td>Austria</td></tr><tr><td>Island Trading</td><td>Helen Bennett</td><td>UK</td></tr><tr><td>Laughing Bacchus Winecellars</td><td>Yoshi Tannamuri</td><td>Canada</td></tr><tr><td>Magazzini Alimentari Riuniti</td><td>Giovanni Rovelli</td><td>Italy</td></tr></table>"
 
-class Window(QMainWindow, Ui_MainWindow):
+class Editor(QMainWindow, main_window.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.save.triggered.connect(lambda: Controller.savePDF(self))
         self.open.triggered.connect(lambda: Controller.openPDF(self))
         self.insertImage.triggered.connect(lambda: Controller.insertImage(self))
-        self.fontSize.setValidator(onlyInt)
-        self.fontSize.textEdited.connect(lambda: Controller.changeFontSize(self))
+        self.fontSize.setRange(5, 99)
+        self.fontSize.valueChanged.connect(lambda: Controller.changeFontSize(self))
         self.editor.textChanged.connect(lambda: Controller.checkSize(self))
+        self.Merger = Merger()
+        self.merger.triggered.connect(lambda: self.Merger.show())
         
+class Merger(QMainWindow, merger.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.select.clicked.connect(lambda: Controller.getFilePaths(self))
+        self.merge.clicked.connect(lambda: Controller.merge(self))
 
 if __name__ == "__main__":
     app = QApplication([])
-    win = Window()
+    win = Editor()
     win.show()
     sys.exit(app.exec())
